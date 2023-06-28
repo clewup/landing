0a3785da202ee28dc90c3d91dc6e95d536c38431
clew <lewis@clewup.co.uk>
2023-06-28 22:22:20 +0100

    feat(process): add images

--- src/components/ListCard/variants/CaptivatingExperiences.tsx
+++ src/components/ListCard/variants/CaptivatingExperiences.tsx
@@ -1,10 +1,16 @@
 import ListCard, { ListCardProps } from "@/components/ListCard/ListCard";
+import Image from "next/image";
 import React, { FC } from "react";
 
 const CaptivatingExperiences: FC<ListCardProps> = ({ id }) => {
   return (
     <ListCard backgroundColor="bg-primary" id={id}>
-      <></>
+      <Image
+        src="/images/paintbrush.png"
+        alt={""}
+        fill={true}
+        className="absolute"
+      />
     </ListCard>
   );
 };
--- src/components/ListCard/variants/ComplexProblems.tsx
+++ src/components/ListCard/variants/ComplexProblems.tsx
@@ -1,10 +1,16 @@
 import ListCard, { ListCardProps } from "@/components/ListCard/ListCard";
+import Image from "next/image";
 import React, { FC } from "react";
 
 const ComplexProblems: FC<ListCardProps> = ({ id }) => {
   return (
     <ListCard backgroundColor="bg-primary" id={id}>
-      <></>
+      <Image
+        src="/images/rocket.png"
+        alt={""}
+        fill={true}
+        className="absolute"
+      />
     </ListCard>
   );
 };
